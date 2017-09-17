3238949cd7cbfe7a6179aa8083a61c96a8ac1f30
prokhorvlg <vlgstudent036@gmail.com>
2017-09-18 00:40:17 +0800

    Labels

--- js/places.js
+++ js/places.js
@@ -15,14 +15,28 @@ svg.append("rect")
     .attr("fill", "#151515");
 
 // Base coordinates of all 
-var xy = [[0, 0], [233, 175], [146, 43]];
-var starNames = ["America", "Yazhou", "Liberty"];
-var starDesc = ["The pristine home of the Federation",
-   "The cyberpunk megalopolis of the Conglomerate", 
-   "This gun is not authorized to fire at the user//"];
+var xy = [[0, 0], [116, 72], [-73, 41], [300, 72]];
 
-var colors = ["#4cace8",
-   "#fff", 
+var starNames = ["America", 
+    "Yazhou", 
+    "Liberty",
+    "Qian Xuesen",
+    "PLACEHOLDER",
+    "PLACEHOLDER",
+    "PLACEHOLDER",
+    "PLACEHOLDER",
+    "PLACEHOLDER",
+    "PLACEHOLDER",
+    "PLACEHOLDER",
+    "PLACEHOLDER",
+    "PLACEHOLDER"];
+
+var quoteDescs = ["They were once a proud people.<br>They were brave and free.",
+   "The cyberpunk megalopolis of the Conglomerate", 
+   "This gun is not authorized to fire at the user"];
+
+var colors = ["#d32823",
+   "#4cace8", 
    "#984deb"];
 
 var cardColor = (function(){
@@ -41,20 +55,49 @@ var ringColor = (function(){
   }
 })();
 
+var quoteColor = (function(){
+  var a = -1;
+  return function(){
+    a++;
+    return colors[a];
+  }
+})();
+
+var starName = (function(){
+  var a = -1;
+  return function(){
+    a++;
+    return starNames[a];
+  }
+})();
+
+var quoteDesc = (function(){
+  var a = -1;
+  return function(){
+    a++;
+    return quoteDescs[a];
+  }
+})();
+
 // ID Generators
 var mainID = (function(){var a = 0; return function(){return a++}})();
 var starID = (function(){var a = 0; return function(){return "star_" + a++}})();
 var starDotID = (function(){var a = 0; return function(){return "starDot_" + a++}})();
 var cardID = (function(){var a = 0; return function(){return "card_" + a++}})();
 var textID = (function(){var a = 0; return function(){return "text_" + a++}})();
+var textQuoteID = (function(){var a = 0; return function(){return "textQuote_" + a++}})();
+
 var markerID = (function(){var a = 0; return function(){return "marker_" + a++}})();
 
 var ringID = (function(){var a = 0; return function(){return "ring_" + a++}})();
+var barID = (function(){var a = 0; return function(){return "bar_" + a++}})();
 
 var cardTLID = (function(){var a = 0; return function(){return "cardTL_" + a++}})();
 var cardTRID = (function(){var a = 0; return function(){return "cardTR_" + a++}})();
 var cardBLID = (function(){var a = 0; return function(){return "cardBL_" + a++}})();
 var cardBRID = (function(){var a = 0; return function(){return "cardBR_" + a++}})();
+
+textQuoteID
 
 // Generates an increment to pull the next item from the array.
 var listItem = (function(){var a = 0; return function(){return a++}})();
@@ -76,6 +119,19 @@ var circleStarRBefore = 5;
 
 var circleRingR = 35;
 var circleRingRBefore = 8;
+
+var widthTextModule = 300;
+var verticalOffsetTextModule = 10;
+var verticalOffsetTextModuleAfter = -130;
+
+var fontSizeBefore = "16px";
+var fontSizeAfter = "24px";
+
+var widthQuoteModule = widthOfCard;
+var verticalOffsetQuoteModule = 10;
+var verticalOffsetQuoteModuleAfter = -90;
+
+var fontSizeQuote = "14px";
 
 // Invisible Rectangle that intercepts Zoom/pan events across entire screen
 svg.append("rect")
@@ -192,14 +248,39 @@ var starRing = svg.selectAll(".groupNode").append("circle")
   .attr("class", "starCirclesRing")
   .attr("id", ringID);
 
-/*var textCard = svg.selectAll(".groupNode").append("div")
+var textCard = svg.selectAll(".groupNode").append("foreignObject")
   .attr('pointer-events', 'none')
   .style("opacity", 1)
-  .style("background", "white")
-  .style("width", "100px")
-  .style("height", "100px")
-  .html("FIRST LINE <br> SECOND LINE")
-  .attr("id", textID);*/
+  .style("color", "#fff")
+  .attr("width", widthTextModule)
+  .attr("height", "100px")
+  .style("font-size", fontSizeBefore)
+  .html(starName)
+  .attr("id", textID)
+  .attr("transform", "translate(" + ((-widthTextModule / 2)) + ", " + (verticalOffsetTextModule) + ")");
+
+var quoteCard = svg.selectAll(".groupNode").append("foreignObject")
+  .attr('pointer-events', 'none')
+  .style("opacity", 0)
+  .style("color", quoteColor)
+  .attr("width", widthQuoteModule)
+  .attr("height", "100px")
+  .style("font-size", fontSizeQuote)
+  .style("font-family", "'Roboto Condensed', Arial, sans-serif")
+  .style("font-style", "italic")
+  .html(quoteDesc)
+  .attr("id", textQuoteID)
+  .attr("transform", "translate(" + ((-widthQuoteModule / 2)) + ", " + (verticalOffsetQuoteModule) + ")");
+
+var horizontalBar = svg.selectAll(".groupNode").append("rect")
+  .attr('pointer-events', 'none')
+  .style("opacity", "0")
+  .attr("width", widthOfCardBefore - 10)
+  .attr("height", 2)
+  .attr("fill", "#fff")
+  .attr("class", "horizontalBar")
+  .attr("id", barID)
+  .attr("transform", "translate(" + ((-widthOfCardBefore / 2) + 5) + ", " + ((-heightOfCardBefore / 2) + 5) + ")");
 
 // Invisible circles generated to intercept hover event
 var hoverCircle = svg.selectAll(".groupNode").append("circle")
@@ -254,6 +335,7 @@ function handleMouseOverStar(d, i) {
 
   d3.select("#ring_" + this.id).transition()
     .duration(200)
+    .style("opacity", 0.2)
     .attr("r", circleRingR);
 
   d3.select("#card_" + this.id).transition()
@@ -288,14 +370,25 @@ function handleMouseOverStar(d, i) {
     .style("opacity", "1")
     .attr("transform", "translate(" + ((+widthOfCard / 2) -40) + ", " + ((+heightOfCard / 2) -40) + ")");
 
-  /*svg.append("text")
-    .attr("id", "t_" + d.id),
-    .attr("": function() { return xScale(d.x) - 30; }),
-    .attr(y: function() { return yScale(d.y) - 15; })
-    })
-    .text(function() {
-      return [d.x, d.y];  // Value of the text
-    });*/
+  d3.select("#text_" + this.id).transition()
+    .duration(200)
+    .ease(d3.easeExp)
+    .style("font-size", fontSizeAfter)
+    .attr("transform", "translate(" + ((-widthTextModule / 2)) + ", " + (verticalOffsetTextModuleAfter) + ")");
+
+  d3.select("#textQuote_" + this.id).transition()
+    .duration(200)
+    .ease(d3.easeExp)
+    .style("opacity", 1)
+    .attr("transform", "translate(" + ((-widthQuoteModule / 2)) + ", " + (verticalOffsetQuoteModuleAfter) + ")");
+
+  d3.select("#bar_" + this.id).transition()
+    .duration(200)
+    .ease(d3.easeExp)
+    .style("opacity", "1")
+    .attr("width", widthOfCard - 50)
+    .attr("transform", "translate(" + ((-widthOfCard / 2) + 25) + ", " + (verticalOffsetTextModuleAfter + 35) + ")");
+    
 }
 
 // Handles MouseOut event for stars
@@ -311,6 +404,7 @@ function handleMouseOutStar(d, i) {
 
     d3.select("#ring_" + this.id).transition()
     .duration(200)
+    .style("opacity", 1)
     .attr("r", circleRingRBefore);
 
   d3.select("#card_" + this.id).transition()
@@ -343,6 +437,26 @@ function handleMouseOutStar(d, i) {
     .ease(d3.easeExp)
     .style("opacity", "0")
     .attr("transform", "translate(" + ((+widthOfCardBefore / 2) -40) + ", " + ((+heightOfCardBefore / 2) -40) + ")");
+
+  d3.select("#text_" + this.id).transition()
+    .duration(200)
+    .ease(d3.easeExp)
+    .style("font-size", fontSizeBefore)
+    .attr("transform", "translate(" + ((-widthTextModule / 2)) + ", " + (verticalOffsetTextModule) + ")");
+
+  d3.select("#textQuote_" + this.id).transition()
+    .duration(200)
+    .ease(d3.easeExp)
+    .style("opacity", 0)
+    .attr("transform", "translate(" + ((-widthQuoteModule / 2)) + ", " + (verticalOffsetQuoteModule) + ")");
+
+  d3.select("#bar_" + this.id).transition()
+    .duration(200)
+    .ease(d3.easeExp)
+    .style("opacity", "0")
+    .attr("width", widthOfCardBefore - 10)
+    .attr("transform", "translate(" + ((-widthOfCardBefore / 2) + 5) + ", " + ((-heightOfCardBefore / 2) + 5) + ")");
+
 }
 
 function redraw(){
